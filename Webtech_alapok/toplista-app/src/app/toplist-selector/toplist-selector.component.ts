42d10add23af2a5e13ca9de7c029f1713ab4d54c
barpet007 <bartokp007@gmail.com>
2025-05-21 11:29:01 +0200

    Top Lista app

--- Webtech_alapok/toplista-app/src/app/toplist-selector/toplist-selector.component.ts
+++ Webtech_alapok/toplista-app/src/app/toplist-selector/toplist-selector.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Toplist } from './toplist.model';
 
 @Component({
   selector: 'app-toplist-selector',
-  imports: [],
-  templateUrl: './toplist-selector.component.html',
-  styleUrl: './toplist-selector.component.css'
+  template: `
+    <h3>Válassz toplistát!</h3>
+    <ul>
+      <li *ngFor="let list of toplists" (click)="select(list)" [class.selected]="list.id === selectedId">
+        {{list.name}} ({{list.type}})
+      </li>
+    </ul>
+    <button (click)="createNew()">Új toplista létrehozása</button>
+  `,
+  styles: [`.selected { font-weight: bold; }`]
 })
 export class ToplistSelectorComponent {
+  @Input() toplists: Toplist[] = [];
+  @Input() selectedId?: number;
+  @Output() selectToplist = new EventEmitter<Toplist>();
+  @Output() create = new EventEmitter<void>();
 
+  select(list: Toplist) {
+    this.selectToplist.emit(list);
+  }
+  createNew() {
+    this.create.emit();
+  }
 }
