be3baf294c1b7f5f7e7cad3d70341b9a93052da4
barpet007 <bartokp007@gmail.com>
2025-05-21 11:32:26 +0200

    Top List Creator

--- Webtech_alapok/toplista-app/src/app/toplist-create/toplist-create.component.ts
+++ Webtech_alapok/toplista-app/src/app/toplist-create/toplist-create.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Toplist } from '../toplist.model';
 
 @Component({
   selector: 'app-toplist-create',
-  imports: [],
-  templateUrl: './toplist-create.component.html',
-  styleUrl: './toplist-create.component.css'
+  template: `
+    <h3>Új toplista létrehozása</h3>
+    <input [(ngModel)]="name" placeholder="Lista neve">
+    <select [(ngModel)]="type">
+      <option value="film">Film</option>
+      <option value="zene">Zene</option>
+      <option value="sorozat">Sorozat</option>
+    </select>
+    <button (click)="create()">Létrehoz</button>
+  `
 })
 export class ToplistCreateComponent {
+  name = '';
+  type: 'film' | 'zene' | 'sorozat' = 'film';
+  @Output() createToplist = new EventEmitter<Toplist>();
 
+  create() {
+    if (this.name.trim()) {
+      this.createToplist.emit({
+        id: Date.now(),
+        name: this.name,
+        type: this.type,
+        items: []
+      });
+      this.name = '';
+    }
+  }
 }
