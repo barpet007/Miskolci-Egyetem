1cabfefb92a4700a894ff0a97c2fda1432b37e61
barpet007 <bartokp007@gmail.com>
2025-05-21 11:33:51 +0200

    App compones

--- Webtech_alapok/toplista-app/src/app/app.component.ts
+++ Webtech_alapok/toplista-app/src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Toplist } from './toplist.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <app-toplist-selector
+      [toplists]="toplists"
+      [selectedId]="selectedToplist?.id"
+      (selectToplist)="onSelect($event)"
+      (create)="onCreate()">
+    </app-toplist-selector>
+
+    <app-toplist-create *ngIf="creating" (createToplist)="onCreated($event)"></app-toplist-create>
+    <app-toplist-editor *ngIf="selectedToplist && !creating"
+      [toplist]="selectedToplist"
+      (saveToplist)="onSave($event)">
+    </app-toplist-editor>
+  `
 })
 export class AppComponent {
-  title = 'toplista-app';
+  toplists: Toplist[] = [];
+  selectedToplist?: Toplist;
+  creating = false;
+
+  onSelect(list: Toplist) {
+    this.selectedToplist = list;
+    this.creating = false;
+  }
+  onCreate() {
+    this.creating = true;
+    this.selectedToplist = undefined;
+  }
+  onCreated(list: Toplist) {
+    this.toplists.push(list);
+    this.selectedToplist = list;
+    this.creating = false;
+  }
+  onSave(list: Toplist) {
+    // már referenciában szerkesztjük, csak frissítjük a nézetet
+    alert('Mentve!');
+  }
 }
